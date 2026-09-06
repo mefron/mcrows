5d8b73ffca12819c4973f120cb2d06302659dc5a
M <mefron1024@gmail.com>
2026-09-06 13:01:37 -0700

    Added basic data models

--- module/data-model.mjs
+++ module/data-model.mjs
@@ -1,16 +1,23 @@
 const {
-  HTMLField, SchemaField, NumberField, StringField, FilePathField, ArrayField
+  HTMLField, SchemaField, NumberField, StringField, FilePathField, ArrayField, BooleanField
 } = foundry.data.fields;
 
 class CharacterData extends foundry.abstract.TypeDataModel {
 	static defineSchema() {
 		return {
-			biography: new HTMLField(),
-			health: new SchemaField({
+			stamina: new SchemaField({
 				value: new NumberField({ required: true, integer: true, min: 0, initial: 10 }),
 				min: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
 				max: new NumberField({ required: true, integer: true, min: 0, initial: 10 })
 			}),
+		};
+	}
+}
+
+class CrowData extends CharacterData {
+	static defineSchema() {
+		return {
+			biography: new HTMLField(),
 			traits: new ArrayField(new StringField()),
 			expertise: new ArrayField(new StringField()),
 			total_xp: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
@@ -18,3 +25,4 @@ class CharacterData extends foundry.abstract.TypeDataModel {
 		};
 	}
 }
+
